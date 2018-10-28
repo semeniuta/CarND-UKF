effe09e1bafb0921eb9c2c6dfa0c1e9aa73e9b27
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2018-10-28 20:16:47 +0100

    Implementation of CalculateRMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -8,8 +8,37 @@ using std::vector;
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd>& estimations,
                               const vector<VectorXd>& ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+
+  VectorXd result(4);
+  result << 0,0,0,0;
+
+  if (estimations.empty()) {
+    cerr << "Empty estimations vector\n";
+    return result;
+  }
+
+  if (estimations.size() != ground_truth.size()) {
+    cerr << "Vector sizes mismatch\n";
+    return result;
+  }
+
+  unsigned long n = estimations.size();
+
+  for(int i = 0; i < n; i++){
+
+    VectorXd x_hat = estimations[i];
+    VectorXd x_true = ground_truth[i];
+
+    VectorXd diff = x_hat - x_true;
+    VectorXd sq_diff = diff.array() * diff.array();
+
+    result = result + sq_diff;
+  }
+
+  result = result / n;
+
+  result = result.array().sqrt();
+
+  return result;
+
 }
